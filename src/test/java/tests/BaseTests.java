2bf4f059c2f69c1ac29e4eba4749339eb6ead0cc
maxim.nagimov@gmail.com <AKsge9Z9sZVCC@j>
2026-05-03 19:53:39 +0500

    change yaml 6

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -1,25 +1,51 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
-
-import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTests {
-    @BeforeAll
-    static void setUp() {
-        headless = false;
-        browserSize = "2560x1440";
-        browser = "chrome";
-        browserVersion = "latest";
-        baseUrl = "https://github.com";
-        pageLoadStrategy = "eager";
-        timeout = 10000;
-    }
 
-    @AfterEach
-    void down() {
-        closeWebDriver();
+    @BeforeAll
+    static void setup() {
+
+        Configuration.browser = "chrome";
+        Configuration.headless = true;
+        Configuration.timeout = 10000;
+        Configuration.browserSize = "1920x1080";
+        Configuration.savePageSource = true;
+        Configuration.screenshots = true;
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--headless");
+        options.addArguments("--remote-allow-origins=*");
+
+        Configuration.browserCapabilities = options;
+
+//        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+//            String[] possiblePaths = {
+//                    "/usr/bin/chromedriver",
+//                    "/usr/local/bin/chromedriver",
+//                    "/snap/bin/chromedriver"
+//            };
+//
+//            for (String path : possiblePaths) {
+//                if (new java.io.File(path).exists()) {
+//                    System.setProperty("webdriver.chrome.driver", path);
+//                    break;
+//                }
+//            }
+//        }
+
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide()
+                        .screenshots(true)
+                        .savePageSource(true)
+        );
     }
 }
